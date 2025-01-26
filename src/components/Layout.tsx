2302ce8d1005e47ba452f26ea1e4f2daab805f69
shinobiKoda <sirp2804@gmail.com>
2025-01-26 16:11:50 +0100

    project setup

---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import logo from '../assets/images/logo.svg';
+
+const Layout = () => {
+  return (
+    <div>
+      <div>
+        <img src={logo} alt="Logo Image" />
+      </div>
+    </div>
+  )
+}
+
+export default Layout
